0d039c24f313530036ec8b876cf665cf2d67be96
vishal1145 <vishal.kumar1145@gmail.com>
2019-10-17 17:33:55 +0530

    commit fix

---
+++ ASP_Core_Demo/Controllers/TestCoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP_Core_Demo.Models;
+using System.Data;
+
+namespace ASP_Core_Demo.Controllers
+{
+    public class TestCoreController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string getdata = "select * from EmpDemo";
+            DataTable dt = DBManager.ExecuterAddpter(getdata);
+            return View(dt);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            string str = "insert into EmpDemo (Name,Address)Values('" + employee.Name + "','" + employee.Address + "')";
+            DBManager.ExecuteQuery(str);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ASP_Core_Demo/Models/DBManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASP_Core_Demo.Models
+{
+    public class DBManager
+    {
+        static string ConString = @"Data Source=.; Initial Catalog=Demo; Integrated Security=True;";
+
+        public static string ExecuteQuery(string sql)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public static DataTable ExecuterAddpter(string getdata)
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(getdata, ConString);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
